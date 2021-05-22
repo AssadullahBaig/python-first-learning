1e1e7821685a92c404035962e63eeea94ea755d7
Assad <assadullahbaig3@gmail.com>
2021-05-22 22:16:01 +0500

    Ch10(Complete)

--- crash-course-book/ch10.py
+++ crash-course-book/ch10.py
@@ -1,5 +1,114 @@
 # --------- Practice -------------------
+# import json
+# numbers = [2, 3, 5 ,7 , 10]
+# filename = 'numbers.json'
+# with open (filename, 'w') as f:
+#     json.dump(numbers, f)
 
+# import json
+
+# username = input("what is your name? ")
+# filename = 'username.json'
+# with open(filename, 'w') as f:
+#     json.dump(username, f)
+#     print("we'll remember you when you come back")
+
+
+# import json
+
+# filename = 'username.json'
+# with open(filename) as f:
+#     username = json.load(f)
+#     print(f"welcome back , {username}!")
+
+
+# import json
+# Load the username, if it has been stored previously.
+# Otherwise, prompt for the username and store it.
+
+# filename = 'username.json'
+# try:
+#     with open(filename) as f:
+#         username = json.load(f)
+# except FileNotFoundError:
+#     username = input("what is your name? ")
+#     with open (filename, 'w') as f:
+#         json.dump(username, f)
+#         print(f"We'll remember you when you come back, {username}! ")
+# else:
+#     print(f"Welcome back, {username}! ")
+
+# import json
+
+# def greet_user():
+#     filename= 'username.json'
+#     try: 
+#         with open (filename) as f:
+#             username = json.load(f)
+#     except FileNotFoundError:
+#         username = input("What is your name? ")
+#         with open(filename, 'w') as f:
+#             json.dump(username, f)
+#             print(f"We'll remember you when you come back, {username}")
+#     else:
+#         print(f"Welcome back, {username}!")
+# greet_user()
+
+# import json
+
+# def get_Stored_username():
+#     filename = 'username.json'
+#     try:
+#         with open (filename) as f:
+#             username = json.load(f)
+#     except FileNotFoundError:
+#         return None
+#     else:
+#         return username
+
+# def greet_user():
+#     username = get_Stored_username()
+#     if username:
+#         print(f"welcome back, {username}!")
+#     else:
+#         username = input("What is your name? ")
+#         filename = 'username.json'
+#         with open (filename, 'w') as f:
+#             json.dump(username, f)
+#             print(f"We'll remember you when you come back {username}!")
+# greet_user()
+
+
+# import json
+
+# def get_Stored_username():
+#     filename = 'username.json'
+#     try:
+#         with open (filename) as f:
+#             username = json.load(f)
+#     except FileNotFoundError:
+#         return None
+#     else:
+#         return username
+
+# def get_new_username():
+#     username = input("What is your name? ")
+#     filename = 'username.json'
+#     with open(filename, 'w') as f:
+#         json.dump(username, f)
+#     return username
+
+# def greet_user():
+#     username = get_Stored_username()
+#     if username:
+#         print(f"welcome back, {username}!")
+#     else:
+#         username = input("What is your name? ")
+#         filename = 'username.json'
+#         with open (filename, 'w') as f:
+#             json.dump(username, f)
+#             print(f"We'll remember you when you come back {username}!")
+# greet_user()
 
 # --------------------------------------\
 
@@ -36,7 +145,6 @@ with open(filename) as f:
     lines = f.readlines()
 
 for line in lines:
-    # Get rid of newline, then replace Python with C.
     line = line.rstrip()
     print(line.replace('Python', 'C'))
 
@@ -155,3 +263,88 @@ for filename in filenames:
     else:
         print("\nReading file: " + filename)
         print(contents)
+
+# 10-10
+print('\n---10-10---')
+def count_common_words(filename, word):
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        pass
+    else:
+        word_count = contents.lower().count(word)
+
+        msg = f"'{word}' appears in {filename} about {word_count} times."
+        print(msg)
+
+filename = 'book.txt'
+count_common_words(filename, 'the')
+
+# 10-11
+print('\n---10-11---')
+import json
+
+number = input("What's your favorite number? ")
+
+with open('favorite_number.json', 'w') as f:
+    json.dump(number, f)
+    print("Thanks! I'll remember that.")
+
+import json
+
+with open('favorite_number.json') as f:
+    number = json.load(f)
+
+print(f"I know your favorite number! It's {number}.")
+
+# 10-12
+print('\n---10-12---')
+import json
+
+try:
+    with open('favorite_number.json') as f:
+        number = json.load(f)
+except FileNotFoundError:
+    number = input("What's your favorite number? ")
+    with open('favorite_number.json', 'w') as f:
+        json.dump(number, f)
+    print("Thanks, I'll remember that.")
+else:
+    print(f"I know your favorite number! It's {number}.")
+
+# 10-13
+print('\n---10-13---')
+import json
+
+def get_stored_username():
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        correct = input(f"Are you {username}? (y/n) ")
+        if correct == 'y':
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_username()
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+greet_user()
